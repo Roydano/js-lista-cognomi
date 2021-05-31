a3d7f8c751aab89bcee6f55d93620a76b3ebbb28
Roydano <roy.dano90@gmail.com>
2021-05-31 15:28:45 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Mail
+// Chiedi all’utente la sua email,
+//  2.  controlla che sia nella lista di chi può accedere,
+//  3.  stampa un messaggio appropriato sull’esito del controllo.
+
+
+
+// Lista Cognomi:
+// chiedi all’utente il cognome
+// inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+// Bonus: (da fare solo dopo aver concluso tutto) Trovate un modo per trasformare in maiuscolo la prima lettera del cognome inserito dall'utente e tutto il resto in minuscolo.
+
+
