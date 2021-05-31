da85de4b92a948001f8cfb9bb769325df7f2bcbf
Roydano <roy.dano90@gmail.com>
2021-05-31 17:01:40 +0200

    inizio esercizio cognomi

---
+++ cognomi/js/script.js
@@ -0,0 +1,7 @@
+
+// Lista Cognomi:
+// chiedi all’utente il cognome
+// inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+// Bonus: (da fare solo dopo aver concluso tutto) Trovate un modo per trasformare in maiuscolo la prima lettera del cognome inserito dall'utente e tutto il resto in minuscolo.
